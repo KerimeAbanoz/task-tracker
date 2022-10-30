d871630ca2f1df7444c1dab671b668697405f77a
kerime abanoz <krmeabnz@gmail.com>
2022-10-30 20:04:43 +0300

    Project started

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TaskList from "../taskList/TaskList";
+
+
+const Header = () => {
+  return (
+    <div>
+      <h1>TASK TRACKER</h1>
+    </div>
+  );
+};
+
+export default Header;
