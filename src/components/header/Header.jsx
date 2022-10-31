ae8404302f6aa748d2f5513ffbd28cbd457393b6
kerime abanoz <krmeabnz@gmail.com>
2022-10-31 16:16:34 +0300

    all done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,10 @@ import TaskPng from "../../assets/TaskPng";
 
 const Header = () => {
   return (
-    <div>
-      <div className="d-flex justify-content-evenly align-items-center">
-        <TaskPng />
-        <h1 className="display-5">TASK TRACKER</h1>
-      </div>
+    <div className="d-flex justify-content-evenly align-items-center">
+      <TaskPng />
+      <h1 className="display-5">TASK TRACKER</h1>
     </div>
   );
 };
-
 export default Header;
