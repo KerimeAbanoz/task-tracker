046631af114cd795e50de33d418791df238fec29
kerime abanoz <krmeabnz@gmail.com>
2022-10-31 13:44:19 +0300

    tasklist

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState("Show Task Bar");
+  const [task, setTask] = useState([]);
+  const url = "https://635ea26ded25a0b5fe4a2226.mockapi.io/api/tasks";
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    const buttonText = isOpen ? "Show Task Bar" : "Hide Task Bar";
+    setText(buttonText);
+  };
+
+  // CRUD  -READ--
+  const getTask = async () => {
+    const { data } = await axios(url);
+    setTask(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getTask();
+  }, []);
+
   return (
-    <div>
-      <Button variant="danger">Show Task Bar</Button>
-      <AddTask />
-      <TaskList />
+    <div className="mt-4 d-flex justify-content-center flex-column">
+      <Button
+        onClick={() => {
+          toggle();
+        }}
+        variant="danger"
+      >
+        {text}
+      </Button>
+      {isOpen && <AddTask getTask={getTask} />}
+      <TaskList task={task} getTask={getTask} />
     </div>
   );
 };
 
 export default Home;
+
+
